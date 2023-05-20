c4dcd2dad38d6b6f62798513025fa9860462101d
Randy Eckenrode <randy@largeandhighquality.com>
2023-05-20 12:08:35 -0400

    darwin.top: fix build with newer clang

--- pkgs/os-specific/darwin/apple-source-releases/top/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/top/default.nix
@@ -5,7 +5,7 @@ appleDerivation {
   buildInputs = [ apple_sdk.frameworks.IOKit ncurses libutil ];
   # Workaround build failure on -fno-common toolchains:
   #   duplicate symbol '_tsamp' in: main.o top.o
-  env.NIX_CFLAGS_COMPILE = "-fcommon";
+  env.NIX_CFLAGS_COMPILE = toString [ "-fcommon" "-Wno-switch-enum" ];
   NIX_LDFLAGS = "-lutil";
   installPhase = ''
     install -D Products/Release/libtop.a $out/lib/libtop.a
