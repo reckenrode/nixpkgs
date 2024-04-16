e0c85d5bb527d7d57e738c104c413986edc93c9a
Randy Eckenrode <randy@largeandhighquality.com>
2024-04-16 00:26:01 -0400

    homepage-dashboard: use top-level cctools

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5484,7 +5484,6 @@ with pkgs;
   hocr-tools = with python3Packages; toPythonApplication hocr-tools;
 
   homepage-dashboard = callPackage ../servers/homepage-dashboard {
-    inherit (darwin) cctools;
     inherit (darwin.apple_sdk.frameworks) IOKit;
   };
 
