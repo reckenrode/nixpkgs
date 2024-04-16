e5283f7ad56223327937baf092d134d9e21f9bc5
Randy Eckenrode <randy@largeandhighquality.com>
2024-04-16 00:02:32 -0400

    python3Packages.tensorflow-probability: use top-level cctools

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15017,9 +15017,7 @@ self: super: with self; {
 
   tensorflow-metadata = callPackage ../development/python-modules/tensorflow-metadata { };
 
-  tensorflow-probability = callPackage ../development/python-modules/tensorflow-probability {
-    inherit (pkgs.darwin) cctools;
-  };
+  tensorflow-probability = callPackage ../development/python-modules/tensorflow-probability { };
 
   tensorflow = self.tensorflow-build;
 
