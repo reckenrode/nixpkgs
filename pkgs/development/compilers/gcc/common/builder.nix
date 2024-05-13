bd44eda25690d9498edde4cd564bf4963e9cc8ad
Randy Eckenrode <randy@largeandhighquality.com>
2024-05-13 19:46:19 -0400

    gcc: drop rpath from build on platforms that don’t set rpaths
    Darwin uses absolute path install names, so setting rpaths is unnecessary. This fixes breakage on older versions of GCC that try to set a deployment target that does not support rpaths.

--- pkgs/development/compilers/gcc/common/builder.nix
+++ pkgs/development/compilers/gcc/common/builder.nix
@@ -20,8 +20,15 @@ originalAttrs: (stdenv.mkDerivation (finalAttrs: originalAttrs // {
 
     if test "$staticCompiler" = "1"; then
         EXTRA_LDFLAGS="-static"
+    ${
+      # todo(@reckenrode) This is ugly, but it avoid rebuilds on Linux. Remove after the current staging-next cycle.
+      lib.optionalString stdenv.buildPlatform.isDarwin ''
+    elif test "$NIX_DONT_SET_RPATH" != "1"; then
+    ''}    EXTRA_LDFLAGS="-Wl,-rpath,''${!outputLib}/lib"
+    ${
+      lib.optionalString stdenv.buildPlatform.isDarwin ''
     else
-        EXTRA_LDFLAGS="-Wl,-rpath,''${!outputLib}/lib"
+    ''}    EXTRA_LDFLAGS=""
     fi
 
     # GCC interprets empty paths as ".", which we don't want.
@@ -56,8 +63,12 @@ originalAttrs: (stdenv.mkDerivation (finalAttrs: originalAttrs // {
                 extraLDFlags=("-L/usr/lib64" "-L/usr/lib")
                 libc_libdir="/usr/lib"
             fi
-            extraLDFlags=("-L$libc_libdir" "-rpath" "$libc_libdir"
-                          "''${extraLDFlags[@]}")
+            extraLDFlags=("-L$libc_libdir")
+            nixDontSetRpathVar=NIX_DONT_SET_RPATH''${post}
+            if test "''${!nixDontSetRpathVar}" != "1"; then
+                extraLDFlags+=("-rpath" "$libc_libdir")
+            fi
+            extraLDFlags+=("''${extraLDFlags[@]}")
             for i in "''${extraLDFlags[@]}"; do
                 declare -g EXTRA_LDFLAGS''${post}+=" -Wl,$i"
             done
