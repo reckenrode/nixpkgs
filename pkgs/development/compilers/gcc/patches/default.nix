a93f34db0526ec83fce58e10b1577daeb251c957
Randy Eckenrode <randy@largeandhighquality.com>
2024-05-14 23:01:29 -0400

    gcc13: fix cross-compilation from Darwin with clang 18
    Darwin is treating `NULL` as a `long`, which results in build failures due to no conversion from `long` to pointer types.

--- pkgs/development/compilers/gcc/patches/default.nix
+++ pkgs/development/compilers/gcc/patches/default.nix
@@ -202,6 +202,11 @@ in
   }
 )
 
+# Fix compilation failures on Darwin when building a cross-compiler.
+++ optional (buildPlatform.isDarwin && buildPlatform != targetPlatform) ({
+  "13" = [ ./13/darwin-nullptr.patch ];
+}.${majorVersion} or [])
+
 
 ## Windows
 
