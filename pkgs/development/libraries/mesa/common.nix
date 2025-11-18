450523cf72134cd9744063dd66a54eac915a9313
Randy Eckenrode <randy@largeandhighquality.com>
2025-11-17 20:25:22 -0500

    mesa: update to HEAD

--- pkgs/development/libraries/mesa/common.nix
+++ pkgs/development/libraries/mesa/common.nix
@@ -5,14 +5,14 @@
 # nix build .#legacyPackages.aarch64-darwin.mesa
 rec {
   pname = "mesa";
-  version = "26.2.2";
+  version = "26.2.2-unstable-2026-09-02";
 
   src = fetchFromGitLab {
     domain = "gitlab.freedesktop.org";
     owner = "mesa";
     repo = "mesa";
-    rev = "mesa-${version}";
-    hash = "sha256-IzQjO9q/GWJyBGwbye5gd2nPaheiTHoK2eR/J6QLMCI=";
+    rev = "946b15ec48a54142efc15b97aa44b06df74f2062";
+    hash = "sha256-DrfVF3/6sRNekIu+hfYzXKp9aPgAb3e/J9oiaHQFFuU=";
   };
 
   meta = {
